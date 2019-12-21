e33a88ae2b9100dfa3db21980f93797fe5854c6b
belohnung <projektarbeityt@gmail.com>
2019-12-22 00:49:15 +0100

    Optimised downloading and added a function to continue the download

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,77 @@
 use indicatif::{ProgressBar, ProgressStyle};
-use lust::get_file_size;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::process::exit;
-use std::time::Duration;
-use std::{env, io, thread};
+use std::{env};
+use std::io::{Read, BufWriter, Write, Seek};
+use lust::get_file_size;
+use std::io::SeekFrom::Start;
 
 mod crypt;
 mod vivo;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("Usage: {} <link to vivo> <save as>", args[0]);
+    if args.len() < 4 {
+        println!("Usage: {} <link to vivo> <save as> <continue>", args[0]);
         exit(1)
     }
+
     let url: String = args[1].to_string();
     let save_as: String = args[2].to_string();
+    let should_continue: String = args[3].to_string();
     let site = vivo::Site::new_from_url(url);
-    let mut file: File = File::create(save_as.clone()).unwrap();
-    let total_size = site.get_video_size() as u64;
-    thread::spawn(move || {
-        let pb = ProgressBar::new(total_size);
-        pb.set_style(ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.green/grey}] {bytes}/{total_bytes} ({eta})")
-            .progress_chars("#>-"));
-        let fs = get_file_size(save_as.clone().as_str());
-        while fs < total_size {
-            let fs = get_file_size(save_as.as_str());
-            pb.set_position(fs);
-            thread::sleep(Duration::from_millis(1000));
+    let file;
+    if let Ok(opened_file) =  OpenOptions::new().write(true).read(true).open(&save_as) {
+        println!("⚠ | File already exists");
+        file = opened_file;
+    }else{
+        file =  File::create(save_as.clone()).expect(&format!("Could not create file '{}'", save_as.clone()));
+    }
+    let total_size = reqwest::get(site.get_video_url().as_str()).expect("Could not make request to get size").content_length().expect("Could not get content length");
+    let mut cursor = 0;
+    let mut chunk_size = 2048;
+    if should_continue.eq("true"){
+        let file_size = get_file_size(&save_as);
+        if &file_size + &chunk_size >= total_size {
+            println!("⚠ | File is already fully downloaded");
+            return;
         }
-    });
-    let mut r = reqwest::get(site.get_video_url().as_str()).unwrap();
-    io::copy(&mut r, &mut file);
-    println!("done.")
+        if  &file_size > &0 {
+            println!("⚠ | Continuing download at {} bytes",file_size);
+            cursor = file_size;
+        }
+    }
+
+
+
+        let progress_bar = ProgressBar::new(total_size);
+        progress_bar.set_style(ProgressStyle::default_bar()
+            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.green/grey}] {bytes}/{total_bytes}({bytes_per_sec}) ({eta})")
+            .progress_chars("#>-"));
+        let mut r = reqwest::get(site.get_video_url().as_str()).unwrap();
+
+        let mut writer = BufWriter::new(file.try_clone().expect("Could not clone file"));
+        if cursor > 0{
+            let mut tempbuf = vec![0u8; cursor as usize];
+            r.read_exact(&mut tempbuf).unwrap();
+        }
+        writer.seek(Start(cursor)).expect("Could not seek to continue download");
+        while cursor < total_size {
+
+            if cursor + chunk_size > total_size {
+                chunk_size = cursor + chunk_size - total_size;
+                cursor += chunk_size;
+                println!("⚠ | Adjusted chunk size to {} bytes", &chunk_size)
+            }else{
+                cursor += chunk_size;
+            }
+            let mut tempbuf = vec![0u8; chunk_size as usize];
+            if let Err(_) = r.read_exact(&mut tempbuf){
+                break;
+            }
+            writer.write(&tempbuf).expect("Could not write to file");
+            progress_bar.set_position(cursor);
+
+        }
+        println!("done.")
 }
